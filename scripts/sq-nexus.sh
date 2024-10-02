b4b000f23fa685ca9445e4ac7edcf66a7c7278cd
prashanth <devopstrainings2024@gmail.com>
2024-10-02 04:10:05 +0000

    first commit - infra

---
+++ scripts/sq-nexus.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo docker run --name nexus -d -p 8081:8081 sonatype/nexus3
+sudo docker run --name sonarqube -d -p 9000:9000 sonarqube
